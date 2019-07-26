b59f74097e17b1ad0b84c5925cc1d45ec74f5f62
栗崎一真 <kurisakikazuma@KoheinoMacBook-Pro.local>
2019-07-26 23:41:38 +0900

    complete canvas

--- js/image.js
+++ js/image.js
@@ -1,16 +1,23 @@
 let upobj = document.getElementById("sampleImage");
 upobj.addEventListener("change", function(evt){
+  let img = null;
+  let canvas = document.getElementById("canvas")
   let file = evt.target.files;
   // file reader の作成
   let reader = new FileReader();
   // 画像形式で読み込み
   reader.readAsDataURL(file[0]);
-  // blabURL取得
-  let blobUrl = window.URL.createObjectURL(file[0]);
+
   // 読み込み終了の処理
   reader.onload = function(ev){
-    // 画像エリアに表示する
-    let img = document.getElementById("imagePreview");
-    img.src = blobUrl;
+    img = new Image();
+    img.onload = function () {
+      //キャンバスに画像をセット
+      let context = canvas.getContext("2d");
+      let width = img.width;
+      let height = img.height;
+      context.drawImage(img, 0, 0);
+    }
+  img.src = reader.result;
   }
 }, false);
