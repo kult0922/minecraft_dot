905c24c93e25540918f89459af08784400748cb9
栗崎一真 <kurisakikazuma@KoheinoMacBook-Pro.local>
2019-07-27 00:46:18 +0900

    complete filtter

--- js/image.js
+++ js/image.js
@@ -1,3 +1,16 @@
+//グレースケール変換関数
+const grayFilter = function(src, dst, width, height) {
+  for (var i = 0; i < height; i++) {
+      for (var j = 0; j < width; j++) {
+          var idx = (j + i * width) * 4;
+          var gray = (src[idx] + src[idx + 1] + src[idx + 2]) / 3;
+          dst[idx] = gray;
+          dst[idx + 1] = gray;
+          dst[idx + 2] = gray;
+          dst[idx + 3] = src[idx + 3];
+      }
+  }
+};
 let upobj = document.getElementById("sampleImage");
 upobj.addEventListener("change", function(evt){
   // 画像
@@ -20,6 +33,15 @@ upobj.addEventListener("change", function(evt){
       canvas.width = width;
       canvas.height = height;
       context.drawImage(img, 0, 0);
+
+      //filtter
+      let srcData = context.getImageData(0, 0, width, height);
+      let dstData = context.createImageData(width, height);
+      let src = srcData.data;
+      let dst = dstData.data;
+      grayFilter(src, dst, width, height);
+      context.putImageData(dstData, 0, 0);
+    
     }
   img.src = reader.result;
   }
