4e8cdd363dd41144ade26707752ec277e88ca7c0
栗崎一真 <kurisakikazuma@KoheinoMacBook-Pro.local>
2019-07-27 00:25:12 +0900

    complete canvas

--- js/image.js
+++ js/image.js
@@ -1,21 +1,24 @@
 let upobj = document.getElementById("sampleImage");
 upobj.addEventListener("change", function(evt){
+  // 画像
   let img = null;
+  // キャンバスを取得
   let canvas = document.getElementById("canvas")
+  // データ読み込み
   let file = evt.target.files;
-  // file reader の作成
   let reader = new FileReader();
-  // 画像形式で読み込み
   reader.readAsDataURL(file[0]);
 
   // 読み込み終了の処理
   reader.onload = function(ev){
     img = new Image();
     img.onload = function () {
-      //キャンバスに画像をセット
+      // キャンバスに画像をセット
       let context = canvas.getContext("2d");
       let width = img.width;
       let height = img.height;
+      canvas.width = width;
+      canvas.height = height;
       context.drawImage(img, 0, 0);
     }
   img.src = reader.result;
