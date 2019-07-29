09002172028b99bfa05813909c24933f43f68bae
栗崎一真 <kurisakikazuma@KoheinoMacBook-Pro.local>
2019-07-29 18:11:05 +0900

    cvMAT success

--- js/image.js
+++ js/image.js
@@ -1,53 +1,37 @@
-//グレースケール変換関数
-const grayFilter = function(src, dst, width, height) {
-  for (var i = 0; i < height; i++) {
-      for (var j = 0; j < width; j++) {
-          var idx = (j + i * width) * 4;
-          var gray = (src[idx] + src[idx + 1] + src[idx + 2]) / 3;
-          dst[idx] = gray;
-          dst[idx + 1] = gray;
-          dst[idx + 2] = gray;
-          dst[idx + 3] = src[idx + 3];
-      }
-  }
-};
 //opencv 読み込み確認関数
 function onOpenCvReady() {
   document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
 }
 
-let upobj = document.getElementById("sampleImage");
-upobj.addEventListener("change", function(evt){
-  // 画像
-  let img = null;
-  // キャンバスを取得
-  let canvas = document.getElementById("canvas")
-  // データ読み込み
-  let file = evt.target.files;
-  let reader = new FileReader();
-  reader.readAsDataURL(file[0]);
+let imgElement = document.getElementById('imgSrc');
+let inputElement = document.getElementById('fileInput');
 
-  // 読み込み終了の処理
-  reader.onload = function(ev){
-    img = new Image();
-    img.onload = function () {
-      // キャンバスに画像をセット
-      let context = canvas.getContext("2d");
-      let width = img.width;
-      let height = img.height;
-      canvas.width = width;
-      canvas.height = height;
-      context.drawImage(img, 0, 0);
-
-      //filtter
-      let srcData = context.getImageData(0, 0, width, height);
-      let dstData = context.createImageData(width, height);
-      let src = srcData.data;
-      let dst = dstData.data;
-      grayFilter(src, dst, width, height);
-      context.putImageData(dstData, 0, 0);
-    
-    }
-  img.src = reader.result;
-  }
+inputElement.addEventListener('change', function(evt){
+  imgElement.src = URL.createObjectURL(evt.target.files[0]);
 }, false);
+
+function grayscale(){
+  let mat = cv.imread(imgElement);
+  var dst = new cv.Mat();
+  cv.cvtColor(mat, dst, cv.COLOR_RGBA2GRAY, 0);
+  cv.imshow('canvasOutput', dst);
+  mat.delete();
+  dst.delete();
+}
+
+function test(){
+  let src = cv.imread(imgElement);
+  let dst = src.clone();
+
+  for (let y = 0; y < src.rows; y++) {
+    for (let x = 0; x < src.cols; x++) {
+      dst.ucharPtr(y, x)[0] = 255;
+      dst.ucharPtr(y, x)[1] = 0;
+      dst.ucharPtr(y, x)[2] = 0;
+      dst.ucharPtr(y, x)[3] = 255;
+    } 
+  }
+  cv.imshow('canvasOutput', dst);
+  src.delete();
+  dst.delete();
+}
